37feea7492e066465d867adbc60f07b38ff0f4f0
Muslum Agah <muslumagah7@gmail.com>
2024-01-12 19:13:49 +0300

    ...

--- KDVCalculator.java
+++ KDVCalculator.java
@@ -5,8 +5,19 @@ public class KDVCalculator {
         Scanner scan = new Scanner(System.in);
         System.out.print("Enter your amount of money: ");
         double a = scan.nextDouble();
-        double a = scan.nextDouble();
+        double v = scan.nextDouble();
 
+
+
+
+
+
+        System.out.println();
+
+        System.out.println();
+        System.out.println();
+        System.out.println();
+        System.out.println();
     
      
     }
