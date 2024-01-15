1633aec259539267c3e7c9513958ecfbde9eff84
Muslum Agah <muslumagah7@gmail.com>
2024-01-15 17:47:20 +0300

    Fixed SmallestAndBiggestNumbers

--- SmallestAndBiggestNumbers.java
+++ SmallestAndBiggestNumbers.java
@@ -8,15 +8,15 @@ public class SmallestAndBiggestNumbers {
         System.out.print("How many numbers will you enter: ");
         int count = inp.nextInt();
         for (int i = 0; i < count; i++) {
-            System.out.print("Enter " + i + ". number: ");
+            System.out.print("Enter " + (i + 1) + ". number: ");
             int number1 = inp.nextInt();
             if (number1 > maxValue)
                 maxValue = number1;
             if (number1 < minValue)
                 minValue = number1;
         }
-        System.out.println("Smallest number is: "+minValue);
-        System.out.println("Biggest number is: "+maxValue);
+        System.out.println("Smallest number is: " + minValue);
+        System.out.println("Biggest number is: " + maxValue);
 
     }
 }
